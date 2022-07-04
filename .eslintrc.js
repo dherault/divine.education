0f0c878b58ccf7b665a993c940a734a0a0937fbe
David Hérault <dherault@gmail.com>
2022-07-04 05:00:07 +0300

    Bootstrap application :tada:

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: [
+    'dherault',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    sourceType: 'module',
+    babelOptions: {
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+      ],
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  globals: {
+    JSX: true,
+    jest: true,
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-no-bind': 'off',
+    'react/require-default-props': 'off',
+    'react/destructuring-assignment': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+}
